659f9d171c71ca01bd084aae7266911525505b20
cenchuan <cenchuan2013@gmail.com>
2023-02-26 16:35:52 +0800

    first

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -24,6 +26,9 @@ def index():
 
 
 def generate_prompt(animal):
-    return """I want you to act as a travel guide. I will write you my location and you will suggest a place to visit near my location. In some cases, I will also give you the type of places I will visit. You will also suggest me places of similar type that are close to my first location. My first suggestion request is "{}".请用中文回复。""".format(
+    return """I want you to act as a travel guide. I will write you my location and you will suggest a place to visit near my location. In some cases, I will also give you the type of places I will visit. You will also suggest me places of similar type that are close to my first location. My first suggestion request is "{}".请用中文回复。字数要求200字以上。""".format(
         animal.capitalize()
     )
+
+if __name__ == '__main__':
+   app.run(host="192.168.0.3",port=80)
